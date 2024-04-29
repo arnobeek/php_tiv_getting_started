9798043086c400449a49124913d25fc1e619ad71
kal-J <kharljay15@gmail.com>
2024-04-29 13:58:40 +0300

    chore: save data into mysql DB

--- include.php
+++ include.php
@@ -9,14 +9,14 @@
         $fpo = $_POST['fpo'];
 
         //Database connection
-        $conn = new mysqli('localhost','root','','sample_php');
+        $conn = new mysqli('localhost','root','password','php_crash');
         if ($conn->connect_error) {
             die('Connection Failed : '. $conn->connect_error);
         }else{
-            $stmt = $conn->prepare("insert into registration values($fname,$lname,$email,$pnumber,$password,    $cpassword,$user,$fpo)");
-            $stmt->bind_param("sssissss",$fname, $lname, $email, $pnumber, $password, $cpassword, $user, $fpo);
+            $stmt = $conn->prepare("insert into user set first_name='$fname', last_name='$lname'");
+            //$stmt->bind_param("sssissss",$fname, $lname, $email, $pnumber, $password, $cpassword, $user, $fpo);
             $stmt->execute();
-            echo"registration successful";
+            echo"\nregistration successful";
             $stmt->close();
             $conn->close();
 
